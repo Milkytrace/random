fa563eb129a335a2245bb4cd59b8993a817f2267
Yutung Huang <milkytrace@hotmail.com>
2018-09-25 16:25:16 +0800

    let user decide min and max

--- import.py
+++ import.py
@@ -1,13 +1,20 @@
 import random
 
 print('\n這是一個猜數字的遊戲\n')
-r = random.randint(1,100)
+min = input('請輸入最低範圍（含）')
+min = int(min)
+max = input('請輸入最高範圍(含）')
+max = int(max)
+r = random.randint(min,max)
+i = 0
 while True:
-	user = input('\n請輸入你猜的數字（在1-100之間，包括）：')
+	i = i + 1
+	user = input('\n請輸入你猜的數字：')
 	if int(user) > r:
 		print('\n太大了')
 	elif int(user) < r:
 		print('\n太小了')
 	else:
-		print('\n你猜對了！')
+		print('\n你猜對了嘿嘿嘿！')
+		print('\n你一共猜了',i,'次哦！')
 		break
